a767ee7e00119c4372ba35dcbf7d02ede49ba2c1
veerapongs <98440031+veerapongs@users.noreply.github.com>
2022-01-26 11:09:36 +0700

    update hello

--- menu.py
+++ menu.py
@@ -1,5 +1,8 @@
 print('manin menu')
 print('hello')
+print('hello2')
+print('hello3')
+print('hello4')
 
 git config --global user.email "ratthaslip.booster@gmail.com"
 git config --global user.email "iotinvationlab"
